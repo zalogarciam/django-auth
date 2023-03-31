7c45ef22674a9e3da0e6056c69089fa230a271f7
zalogarciam <gegarciam95@gmail.com>
2023-03-30 20:23:41 -0500

    Create POST user

--- vet/gestion/views.py
+++ vet/gestion/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegistroUsuarioSerializer
+from .models import Usuario
+class RegistroUsuario(APIView):
+    def post(self, request: Request): 
+        serializador = RegistroUsuarioSerializer(data = request.data)
+        if serializador.is_valid():
+            password = serializador.validated_data.get('password')
 
-# Create your views here.
+            nuevo_usuario = Usuario(**serializador.validated_data)
+            
+            nuevo_usuario.set_password(password)
+            nuevo_usuario.save()
+
+            return Response(
+                data = {
+                'message': 'Usuario creado'
+                }, status=status.HTTP_201_CREATED
+            )
+        else:
+            return Response(
+                data = {
+                'message': 'Error crear usuario',
+                'content': serializador.errors
+                }, status=status.HTTP_400_BAD_REQUEST
+            )
